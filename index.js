2c7c847f2459548c22e892cd73addbfe2f4c3ddb
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-12-31 21:31:27 +0700

    feat: add file upload functionality with multer

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+	res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+	if (!req.file) {
+		return res.status(400).json({ error: "Please upload a file" });
+	}
 
-
+	res.json({
+		name: req.file.originalname,
+		type: req.file.mimetype,
+		size: req.file.size,
+	});
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, () => {
+	console.log("Your app is listening on port " + port);
 });
